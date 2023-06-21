a6b969a1e76df48a7674d1d5e0f3be62ce2c72ab
hilaman1 <hilaman1@mail.tau.ac.il>
2023-06-21 18:30:56 +0300

    trying to use grad cam

--- testing_model.py
+++ testing_model.py
@@ -3,8 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from PIL import Image
+import torch
+import pytorch_grad_cam
+import torchvision
 
-#TODO: check if needed in order to use grad_cam
+# TODO: check if needed in order to use grad_cam
 '''
 import torch
 import requests
@@ -17,6 +20,7 @@ from pytorch_grad_cam.utils.model_targets import FasterRCNNBoxScoreTarget
 from pytorch_grad_cam.utils.reshape_transforms import fasterrcnn_reshape_transform
 from pytorch_grad_cam.utils.image import show_cam_on_image, scale_accross_batch_and_channels, scale_cam_image
 '''
+
 
 def show_image_with_yoloface_bboxes(image_path, bboxes):
     coordinates = bboxes[0]
@@ -56,8 +60,90 @@ bboxes, points = model.predict(orgimg)
 
 show_image_with_yoloface_bboxes(image_path, bboxes)
 
+detector = model.detector.model
+# get the first conv layer
+last_conv_layer = detector[-2].cv3
 
 
+
+class FasterRCNNBoxScoreTarget:
+    """ For every original detected bounding box specified in "bounding boxes",
+    	assign a score on how the current bounding boxes match it,
+    		1. In IOU
+    		2. In the classification score.
+    	If there is not a large enough overlap, or the category changed,
+    	assign a score of 0.
+
+    	The total score is the sum of all the box scores.
+    """
+
+    def __init__(self, labels, bounding_boxes, iou_threshold=0.5):
+        self.labels = labels
+        self.bounding_boxes = bounding_boxes
+        self.iou_threshold = iou_threshold
+
+    def __call__(self, model_outputs):
+        output = torch.Tensor([0])
+        if torch.cuda.is_available():
+            output = output.cuda()
+
+        if len(model_outputs["boxes"]) == 0:
+            return output
+
+        for box, label in zip(self.bounding_boxes, self.labels):
+            box = torch.Tensor(box[None, :])
+            if torch.cuda.is_available():
+                box = box.cuda()
+
+            ious = torchvision.ops.box_iou(box, model_outputs["boxes"])
+            index = ious.argmax()
+            if ious[0, index] > self.iou_threshold and model_outputs["labels"][index] == label:
+                score = ious[0, index] + model_outputs["scores"][index]
+                output = output + score
+        return output
+
+
+targets = [FasterRCNNBoxScoreTarget(labels=['face'], bounding_boxes=bboxes)]
+
+
+def fasterrcnn_reshape_transform(x):
+    target_size = x['pool'].size()[-2:]
+    activations = []
+    for key, value in x.items():
+        activations.append(
+            torch.nn.functional.interpolate(
+                torch.abs(value),
+                target_size,
+                mode='bilinear'))
+    activations = torch.cat(activations, axis=1)
+    return activations
+
+
+backbone = model.detector.yaml['backbone']
+
+# convert state dict to tensor
+# backbone = torch.load(model.detector.model.state_dict(), map_location=torch.device('cpu'))
+
+cam = pytorch_grad_cam.GradCAM(detector,
+               last_conv_layer,
+               use_cuda=torch.cuda.is_available(),
+               reshape_transform=fasterrcnn_reshape_transform)
+
+transform = torchvision.transforms.Compose([
+    torchvision.transforms.ToTensor(),
+])
+
+# preprocess input image
+input_tensor = transform(orgimg)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+input_tensor = input_tensor.to(device)
+input_tensor = input_tensor.unsqueeze(0)
+input_tensor.requires_grad = True
+
+grayscale_cam = cam(input_tensor)
+
+
+print('hi')
 
 # test grad cam
 # grad cam
